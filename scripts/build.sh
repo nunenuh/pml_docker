ca559462f4640727214435c169c5a46fb36be491
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2021-09-16 06:56:00 +0800

    fix: add changes to build script

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker-compose -f docker-compose.build.stage01.yml build
+
+docker-compose -f docker-compose.build.stage02.yml build
