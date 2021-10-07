3194b40286006b8d48a9141cd0be8d1d3658a2a1
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2021-10-07 15:08:34 +0800

    fix: add deepface model downloader and add remove mlflow from installation

---
+++ python3.8-cpu-base-focal/deepface_model_downloader.py
@@ -0,0 +1,12 @@
+from deepface import DeepFace
+
+if __name__ == '__main__':
+    from deepface import DeepFace
+
+    DeepFace.Age.loadModel()
+    DeepFace.ArcFace.loadModel()
+    DeepFace.Emotion.loadModel()
+    DeepFace.DeepID.loadModel()
+    DeepFace.Facenet.loadModel()
+    DeepFace.Facenet512.loadModel()
+    DeepFace.VGGFace.loadModel()
